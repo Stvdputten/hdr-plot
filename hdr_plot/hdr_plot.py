33688fa9ee881ab652e8a9a2e013bd0434b40afa
stvdputten <stvdputtenjur@gmail.com>
2022-03-12 14:37:07 +0100

    Updated

--- hdr_plot/hdr_plot.py
+++ hdr_plot/hdr_plot.py
@@ -17,6 +17,8 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import seaborn as sns
+import numpy as np
 
 # global settings
 fontsize=20
@@ -40,6 +42,10 @@ def parse_percentiles( file ):
 def parse_files( files ):
     return [ parse_percentiles(file) for file in files]
 
+regex_lines = re.compile(r'(.+((requests)|(99\.000%)).+)')
+regex_val = re.compile(r'requests=([0-9]+)')
+regex_thr = re.compile(r'\s+([0-9]+)\s+requests')
+regex_lat = re.compile(r'.+(99\.000%)+\s+([0-9.\w]+)')
 
 def info_text(name, data):
     textstr = '%-18s\n------------------\n%-6s = %6.2f ms\n%-6s = %6.2f ms\n%-6s = %6.2f ms\n'%(
@@ -79,15 +85,243 @@ def plot_percentiles( percentiles, labels, legend_fontsize=fontsize):
     ax.set_xscale('logit')
     ax.get_xticklabels()[1].set_color("red")
     plt.xticks([0.9, 0.99, 0.999, 0.9999], fontsize=fontsize)
-    # plt.xticks([0.25, 0.5, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999])
     majors = ["90%", "99%", "99.9%", "99.99%", "99.999%", "99.9999%"]
-    # majors = ["25%", "50%", "90%", "99%", "99.9%", "99.99%", "99.999%", "99.9999%"]
 
     ax.xaxis.set_major_formatter(ticker.FixedFormatter(majors))
     ax.xaxis.set_minor_formatter(ticker.NullFormatter())
     plt.legend(labels, loc='upper left', fontsize=legend_fontsize)
 
     return fig, ax
+
+def plot_thr_req(tail_data, labels, legend_location,legend_fontsize=fontsize):
+    # general setup
+    fig, ax = plt.subplots(figsize=(16,8))
+
+    # create new relevant labels
+    data = pd.concat(tail_data)
+    data['filename'] = labels
+    data = data.sort_values(by='Requests').reset_index(drop=True)
+
+    # labels = data['Orchestrator'].unique() 
+    labels = ["swarm","k8s", "nomad"]
+    lines = ["-","--","-.",":"]
+    markers = ["o","v","^","<",">","8","s","p","*","h","H","D","d","P","X"]
+    orchestrator = data['Orchestrator'].unique()[0]
+    benchmark = data['Benchmark'].unique()[0]
+
+    for mark, orch in enumerate(labels):
+        plot_data = data.loc[data['Orchestrator'] == orch]
+        ax.plot(plot_data['Requests'], plot_data['Throughput'], label=f"{benchmark}-{orch}-baseline", linestyle=lines[mark % len(markers)])
+
+    # set axis and legend
+    ax.grid()
+    ax.set_xlim(left=0)
+    ax.set_xlabel('Requests', fontsize=fontsize)
+    ax.set_ylabel('Throughput', fontsize=fontsize)
+    ax.set_title('Throughput versus Requests', fontsize=fontsize)
+
+    xticks = [int(tick) for tick in ax.get_xticks()]
+    plt.yticks(fontsize=fontsize)
+    plt.xticks(xticks, fontsize=fontsize)
+    plt.legend(loc=legend_location, fontsize=legend_fontsize)
+
+    return fig, ax
+
+
+def plot_lat_req_all(tail_data, labels, legend_location,legend_fontsize=fontsize):
+    # general setup
+    fig, ax = plt.subplots(figsize=(16,8))
+
+    # create new relevant labels
+    data = pd.concat(tail_data)
+    data['filename'] = labels
+    data = data.sort_values(by='Requests').reset_index(drop=True)
+
+    # labels
+    labels = ["swarm","k8s", "nomad"]
+    lines = ["-","--","-.",":"]
+    markers = ["o","v","^","<",">","8","s","p","*","h","H","D","d","P","X"]
+    benchmark = data['Benchmark'].unique()[0]
+
+    print("Multiple orchestrators one benchmark and one param")
+    for mark, orch in enumerate(labels):
+        plot_data = data.loc[data['Orchestrator'] == orch]
+        vertical = plot_data['vertical'].unique()[0]
+        horizontal = plot_data['horizontal'].unique()[0]
+        availability = plot_data['availability'].unique()[0]
+        baseline = plot_data['baseline'].unique()[0]
+        if vertical == 0:
+            print("vertical")
+            ax.plot(plot_data['Requests'], plot_data['Latency'], label=f"{benchmark}-{orch}-vertical", linestyle=lines[mark % len(markers)])
+        elif  horizontal == 0:
+            print("horizontal")
+            ax.plot(plot_data['Requests'], plot_data['Latency'], label=f"{benchmark}-{orch}-horizontal", linestyle=lines[mark % len(markers)])
+        elif availability == 1 and baseline == 1:
+            print("availability")
+            ax.plot(plot_data['Requests'], plot_data['Latency'], label=f"{benchmark}-{orch}-availability", linestyle=lines[mark % len(markers)])
+        else:
+            print("baseline")
+            ax.plot(plot_data['Requests'], plot_data['Latency'], label=f"{benchmark}-{orch}-baseline", linestyle=lines[mark % len(markers)])
+
+    # set axis and legend
+    ax.grid()
+    ax.set_xlim(left=0)
+    ax.set_xlabel('Requests', fontsize=fontsize)
+    ax.set_ylabel('Latency (milliseconds)', fontsize=fontsize)
+    ax.set_title('Latency versus Requests', fontsize=fontsize)
+
+    xticks = [int(tick) for tick in ax.get_xticks()]
+    plt.yticks(fontsize=fontsize)
+    plt.xticks(xticks, fontsize=fontsize)
+    plt.legend(loc=legend_location, fontsize=legend_fontsize)
+
+    return fig, ax
+    
+
+def plot_lat_req(tail_data, labels, legend_location,legend_fontsize=fontsize):
+    # general setup
+    fig, ax = plt.subplots(figsize=(16,8))
+
+
+    # create new relevant labels
+    data = pd.concat(tail_data)
+    data['filename'] = labels
+    data = data.sort_values(by='Requests').reset_index(drop=True)
+
+    labels = data['Benchmark'].unique() 
+    lines = ["-","--","-.",":"]
+    markers = ["o","v","^","<",">","8","s","p","*","h","H","D","d","P","X"]
+    orchestrator = data['Orchestrator'].unique()[0]
+
+    # one benchmark but multiple params
+    if len(labels) == 1:
+        print("one benchmark but multiple params")
+        for benchmark in labels:
+            for mark, param in enumerate(['horizontal', 'vertical', 'availability','baseline']):
+                plot_data = data.loc[data['Benchmark'] == benchmark]
+                if len(plot_data[param].unique()) == 0:
+                    continue
+
+                if param == 'availability':
+                    plot_data = plot_data.loc[plot_data[param] == 1]
+                    if plot_data[param].unique() == 1:
+                        ax.plot(plot_data['Requests'], plot_data['Latency'], label=f"{benchmark}-{orchestrator}-{param}", linestyle=lines[mark % len(markers)])
+                else:
+                    plot_data = plot_data.loc[plot_data[param] == 0]
+                    if plot_data[param].unique() == 0:
+                        ax.plot(plot_data['Requests'], plot_data['Latency'], label=f"{benchmark}-{orchestrator}-{param}", linestyle=lines[mark % len(markers)])
+    # multiple benchmarks but one params
+    else:
+        print("Multiple benchmarks but one param")
+        for mark, benchmark in enumerate(labels):
+                plot_data = data.loc[data['Benchmark'] == benchmark]
+                ax.plot(plot_data['Requests'], plot_data['Latency'], label=f"{benchmark}-{orchestrator}", linestyle=lines[mark % len(markers)])
+    # data = pd.concat(tail_data)
+    # ax.plot(data['Requests'], data['Latency'])
+
+    
+    # set axis and legend
+    ax.grid()
+    # sb.despine(ax=ax, offset=0)
+    ax.set_xlim(left=0)
+    ax.set_xlabel('Requests', fontsize=fontsize)
+    ax.set_ylabel('Latency (milliseconds)', fontsize=fontsize)
+    ax.set_title('Latency versus Requests', fontsize=fontsize)
+
+    xticks = [int(tick) for tick in ax.get_xticks()]
+    # print(xticks)
+    # yticks = [int(tick) for tick in ax.get_yticks()]
+    # print(xticks, yticks)
+    # plt.yticks(yticks, fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    # plt.xticks(xticks, fontsize=fontsize)
+    plt.xticks(xticks, fontsize=fontsize)
+    plt.legend(loc=legend_location, fontsize=legend_fontsize)
+
+    return fig, ax
+
+
+
+def save_df(df, filename="default.csv"):
+    df.to_csv(filename, sep=',', index=False)
+
+def parse_files_tail(files):
+    df = pd.concat([ parse_prelim(file) for file in files]).sort_values(by='Requests').reset_index(drop=True)
+    return df
+
+regex_lines = re.compile(r'(.+((requests)|(99\.000%)).+)|(#)|(Req)')
+regex_val = re.compile(r'requests=([0-9]+)')
+regex_thr = re.compile(r'\s+([0-9]+)\s+requests')
+regex_lat = re.compile(r'.+(99\.000%)+\s+([0-9.\w]+)')
+# regex_mean = re.compile('(#\[(Mean\s+=\s+\w+.\w+))')
+
+def parse_prelim(file):
+    # swarm-sn-wrk-mixed-c6525-25g-exp0-havail0-hori1-verti1-infi0-t4-c8-d30-R200
+    # params we need to derive from the file
+    lines = [ line for line in open(file) if re.match(regex_lines, line) ]
+    # print(lines)
+    requests  = float([ val for val in  [ re.findall(regex_val, line) for line in lines ] if len(val) != 0 ][0][0])
+    latency  = [ val for val in [ re.findall(regex_lat, line) for line in lines ][1] if len(val) != 0 ][0][1]
+    if latency[-2] == 'm':
+        latency = float(latency[:-2])
+    else:
+        latency = float(latency[:-1]) * 1000
+    throughput  = float([ val for val in  [ re.findall(regex_thr, line) for line in lines ] if len(val) != 0 ][0][0])
+    regex_mean= "#\[Mean\s+=\s+(\w+.\w+)"
+    mean  = float([ re.search(regex_mean, line) for line in lines if re.search(regex_mean, line) ][0].group(1))
+    regex_max= "#\[Max\s+=\s+(\w+.\w+)"
+    max_val = float([ re.search(regex_max, line) for line in lines if re.search(regex_max, line) ][0].group(1))
+    regex_stddev= ".+Std\w+\s+=\s+(\w+\.\w+)"
+    stddev  = float([ re.search(regex_stddev, line) for line in lines if re.search(regex_stddev, line) ][0].group(1))
+    regex_reqsec= "Requests/.+\W(\w+\.\w+)"
+    reqsec  = float([ re.search(regex_reqsec, line) for line in lines if re.search(regex_reqsec, line) ][0].group(1))
+    # print(requests, latency, throughput, mean, max_val, stddev, reqsec)
+
+    # params stated in the filename
+    # params = str(file).split('/')[-1].split('-')
+    orchestrator =  re.search('(swarm|k8s|nomad)', file).group(1)
+    benchmark = re.search('(sn|hr|mm)', file).group(1)
+    infinite = int(re.search('(infi)(1|0)', file).group(2))
+    exp = re.search('(exp[0-9]+)', file).group(1)
+    availability = int(re.search('(havail)(0|1)', file).group(2))
+    horizontal = int(re.search('(hori)(1|0)', file).group(2))
+    vertical = int(re.search('(verti)(1|0)', file).group(2))
+    threads = int(re.search('(t)([0-9]+)', file).group(2))
+    connections = int(re.search('(c([0-9]+))', file).group(2))
+    duration = int(re.search('d([0-9]+)', file).group(1))
+    if int(availability) == 0 and int(horizontal) == 1 and int(vertical) == 1: 
+        baseline = 0
+    else:
+        baseline = 1
+    # print(orchestrator, benchmark, infinite, exp, availability, horizontal, vertical, threads, connections, duration, latency, throughput, mean, max_val, stddev, reqsec, baseline)
+    # print("orchestrator", "benchmark", "infinite", "exp", "availability", "horizontal", "vertical", "threads", "connections", "duration", "latency", "throughput", "mean", "max", "stddev", "reqsec", "baseline")
+    
+
+
+
+    # df = pd.DataFrame(zip(latency, requests, throughput, benchmark, orchestrator),
+    df = pd.DataFrame({'Latency': [latency],
+        'Requests': [requests], 
+        'Throughput': [throughput],
+        'Baseline': [baseline],
+        'Orchestrator': [orchestrator],
+        'Benchmark': [benchmark],
+        'Infinite': [infinite],
+        'Exp': [exp],
+        'Availability': [availability],
+        'Horizontal': [horizontal],
+        'Vertical': [vertical],
+        'Threads': [threads],
+        'Connections': [connections],
+        'Duration': [duration],
+        'Mean': [mean],
+        'Max': [max_val],
+        'StdDev': [stddev],
+        'ReqSec': [reqsec]}
+    )  
+    print(df.head())
+
+    return df
 
 
 def arg_parse():
@@ -96,8 +330,13 @@ def arg_parse():
     parser.add_argument('--output', default='latency.png',
                         help='Output file name (default: latency.png)')
     parser.add_argument('--title', default='', help='The plot title.')
-    parser.add_argument("--nobox", help="Do not plot summary box",
-                        action="store_true")
+    parser.add_argument("--nobox", help="Do not plot summary box", action="store_true")
+    parser.add_argument('--lat_req', help="Plot latency vs requests", action="store_true")
+    parser.add_argument('--thr_req', help="Plot throughput vs requests", action="store_true")
+    parser.add_argument('--lat_req_all', help="Plot latency vs requests all orchestrators baseline", action="store_true")
+    parser.add_argument('--save', help="Save the dataframe", action="store_true")
+    parser.add_argument('--print', help="Print the head of the dataframe", action="store_true")
+    parser.add_argument('--legend_location', default="lower right", help="Change the legend location")
     parser.add_argument("--legend_fontsize", default=20, help="Change the legend fontsize")
     args = parser.parse_args()
     return args
@@ -106,15 +345,39 @@ def arg_parse():
 def main():
     # print command line arguments
     args = arg_parse()
-    # print(args)
 
     # load the data and create the plot
+    # print(args.files)
+    # return 0
+    df = parse_files_tail(args.files)
+    tail_data = parse_files_tail(args.files)
+
     pct_data = parse_files(args.files)
+    tail_data = parse_files_tail(args.files)
+
     labels = [re.findall(filename, file)[0][1] for file in args.files]
+    # print(labels)
     legend_fontsize = int(args.legend_fontsize)
+    legend_location = args.legend_location
+
+    if args.print:
+        print(df.head())
+        return 0
+
+    if args.save:
+        name = df['Exp'].unique()[0]
+        save_df(df, f'df-{name}.csv')
+        return 0
 
     # plotting data
-    fig, ax = plot_percentiles(pct_data, labels, legend_fontsize)
+    if args.lat_req:
+        fig, ax = plot_lat_req(tail_data, labels, legend_location, legend_fontsize)
+    elif args.lat_req_all:
+        fig, ax = plot_lat_req_all(tail_data, labels, legend_location, legend_fontsize)
+    elif args.thr_req:
+        fig, ax = plot_thr_req(tail_data, labels, legend_location, legend_fontsize)
+    else:
+        fig, ax = plot_percentiles(pct_data, labels, legend_fontsize)
 
     # plotting summary box
     if not args.nobox:
